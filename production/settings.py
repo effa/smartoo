fbc90c760ee533677daf679fa70fa75f3f1d6d7f
effa <tomas.effenberger@gmail.com>
2015-04-02 20:05:17 +0200

    fixed settings.ADMINS

--- production/settings.py
+++ production/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = ['*']
 
 TEMPLATE_DEBUG = True
 
-ADMINS = (('Tomas Effenberger', 'xeffenberger@gmail.com'))
+ADMINS = (('Tomas Effenberger', 'xeffenberger@gmail.com'),)
 
 # Application definition
 
